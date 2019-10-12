9aa4880200b826d44fbbd61be03fd792bcae6626
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-10-12 03:39:41 +0200

    test creating wheels

--- .github/actions/scripts/release.sh
+++ .github/actions/scripts/release.sh
@@ -3,7 +3,6 @@ set -ex
 
 if [[ "refs/tags/$(git tag | grep v | tail -n 1)" = $GITHUB_REF ]]; then
     pip install -U setuptools wheel twine
-    python setup.py sdist bdist_wheel
     python -m twine check dist/*
   if [[ $GITHUB_REF =~ -test$ ]]; then
     python -m twine upload -u pyjnius --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
