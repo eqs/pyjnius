91f644035972c48d40e0deba7e20d9741005d137
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-10-12 18:54:55 +0200

    make sdist release through action as well, on tag

--- .github/actions/scripts/release.sh
+++ .github/actions/scripts/release.sh
@@ -2,11 +2,10 @@
 set -ex
 
 if [[ "refs/tags/$(git tag | grep v | tail -n 1)" = $GITHUB_REF ]]; then
-    pip install -U setuptools wheel twine
     python -m twine check dist/*
   if [[ $GITHUB_REF =~ -test$ ]]; then
-    python -m twine upload -u pyjnius --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
+    python -m twine upload -u kivybot --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
   else
-    python -m twine upload -u pyjnius --disable-progress-bar dist/*
+    python -m twine upload -u kivybot --disable-progress-bar dist/*
   fi
 fi
