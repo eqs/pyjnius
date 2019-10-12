aa9cad814fa219cc08d6475f60ec1e476f237a48
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-10-12 19:25:47 +0200

    try to execute uploader on tag creation

--- .github/actions/scripts/release.sh
+++ .github/actions/scripts/release.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
-if [[ "refs/tags/$(git tag | grep v | tail -n 1)" = $GITHUB_REF ]]; then
-    python -m twine check dist/*
-  if [[ $GITHUB_REF =~ -test$ ]]; then
+python -m twine check dist/*
+if [[ $GITHUB_REF =~ -test$ ]]; then
     python -m twine upload -u kivybot --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
-  else
-    python -m twine upload -u kivybot --disable-progress-bar dist/*
-  fi
+else
+    echo "would have uploaded :|"
+    #python -m twine upload -u kivybot --disable-progress-bar dist/*
 fi
